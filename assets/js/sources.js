bbda419e1a8635a60561b12225d695acc4f2ddc1
jamesdcochran-oss <jamesdcochran@gmail.com>
2025-10-22 20:44:53 -0400

    Delete assets directory

--- assets/js/sources.js
+++
@@ -1,7 +0,0 @@
-export const CONFIG = {
-  bbox: [-78.24, 36.54, -76.96, 37.50],
-  firmsKey: "6ec6f9501dda0774853f77ee933238ed",
-  firmsSource: "VIIRS",
-  firmsHours: 24,
-  airnowKey: "961AB5ED-B1D6-4B5E-8D34-8C097E374925"
-};
